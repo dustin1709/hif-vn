efc429be786177fc5eb3544bfe21135c7b7ba8f5
dustin1709 <duong.chu.vu@lalumiere.org>
2024-10-30 15:35:43 +0700

    first commit

--- src/Careers.js
+++ src/Careers.js
@@ -53,7 +53,7 @@ function Careers() {
                             <div className="p-2 text-center" style={{backgroundColor: '#e0e0eb'}}>
                                 <h2 className="text-3xl">{property.name}</h2>
                             </div>
-                            <div style={{backgroundColor: '#e0e0eb'}} className="rounded-b-xl h-[500px]">
+                            <div style={{backgroundColor: '#e0e0eb'}} className="rounded-b-xl h-[550px]">
                                 <h2 className="pt-3 pl-5 text-2xl">{property.location}</h2>
                                 <ul className="pb-3 pt-2 pl-5 pr-2 text-xl">
                                     <li>{property.sp1}</li>
